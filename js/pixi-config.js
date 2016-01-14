26a760274705172e0bb584fc1ba10746824a41c9
James La Marre <jamesl@stinkdigital.com>
2016-01-14 17:42:21 -0500

    updated to add buttons and variability to waves

--- js/pixi-config.js
+++ js/pixi-config.js
@@ -5,32 +5,29 @@
 var renderer = new PIXI.WebGLRenderer(window.innerWidth, window.innerHeight, { backgroundColor: 0xFFC2FF, antialias: true });
 document.body.appendChild(renderer.view);
 
-var stage = new PIXI.Container;
+var interactive = true;
+var stage = new PIXI.Container(interactive);
+renderer.render(stage);
 
 var waves = new PIXI.Graphics(),
     w = window.innerWidth;
     h = window.innerHeight;
-    // rand = Math.random(); 
-    pi = Math.PI;
-    sin = Math.sin; 
-    duration = 2;
     tick = 0;
-    x = 0;
     points = [],
     opt = {
-      count: 5,
-      range: {
-        x: 150,
-        y: 250
-      },
-      duration: {
-        min: 40,
-        max: 60
-      },
-      thickness: 20,
-      strokeColor: '#FFF',
-      level: .65,
-      curved: true
+		count: 6,
+		range: {
+			x: 50,
+			y: 300
+		},
+		speed: {
+			min: 20,
+			max: 30
+		},
+		duration: 100,
+		strokeColor: '#FFF',
+		level: .80,
+		curved: true
     };
     rand = function(min, max){
         return Math.floor( (Math.random() * (max - min + 1) ) + min);
@@ -40,14 +37,12 @@ var waves = new PIXI.Graphics(),
 	    return -c/2 * ((--t)*(t-2) - 1) + b;
     };
 
-	waves.lineJoin ='round';
-	waves.lineWidth = opt.thickness;
-	waves.strokeStyle = opt.strokeColor;
-
 // 
 
-// create points function to create animation function 
 
+
+
+// create points function to create animation function 
 
 var Point = function(config){
 	this.anchorX = config.x;
@@ -63,7 +58,7 @@ Point.prototype.setTarget = function(){
 	this.targetX = this.anchorX + rand(0, opt.range.x * 2) - opt.range.x;
 	this.targetY = this.anchorY + rand(0, opt.range.y * 2) - opt.range.y;
 	this.tick = 0;
-	this.duration = rand(opt.duration.min, opt.duration.max);
+	this.speed = rand(opt.speed.min, opt.speed.max);
 };
   
 Point.prototype.update = function(){
@@ -77,12 +72,12 @@ Point.prototype.update = function(){
 		var t = this.tick;
 		var b = this.initialY;
 		var c = this.targetY - this.initialY;
-		var d = this.duration;
+		var d = this.speed;
 		this.y = ease(t, b, c, d);
 
 		b = this.initialX;
 		c = this.targetX - this.initialX;
-		d = this.duration;
+		d = this.speed;
 		this.x = ease(t, b, c, d);
 
 		this.tick++;
@@ -91,22 +86,24 @@ Point.prototype.update = function(){
 
 var updatePoints = function(){
 	var i = points.length;
-	while(i--){
+	while(i-- && tick < opt.duration){
 		points[i].update();
 	};
 	return i;
 };
 
-
-
 //
 
 
 
+
+
 // setup waves as PIXI Graphics NEED TO UPDATE TO TEXTURE FOR SMOOTH LINES 
+// startPoint defines Y axis start point
+// counter should be between 4-10
 // color defined by '0xFF00FF' style value
 
-var renderShapes = function(start, color) {
+var renderShapes = function(startPoint, rangeVariation, horizontalVariation, color) {
 
 	var pointCount = points.length;
  	waves.moveTo(points[0].x, points[0].y);
@@ -114,26 +111,33 @@ var renderShapes = function(start, color) {
 
 	var i;
 	for (i = 0; i < pointCount - 1; i++) {
-		var c = (points[i].x + points[i + 1].x) / 2;
-		var d = (points[i].y + points[i + 1].y) / 2;
-		waves.quadraticCurveTo(points[i].x, points[i].y + start, c, d + start);
-	}
 
-	waves.lineTo(-opt.range.x - opt.thickness, h + opt.thickness);
-	waves.lineTo(w + opt.range.x + opt.thickness, h + opt.thickness);
+		var c = points[i].x + (horizontalVariation * 10);
+		var d = points[i].y + startPoint;
+		var e = (points[i].x + points[i + 1].x) / 2;
+		var f = (points[i].y + points[i + 1].y) / 2 + startPoint;
 
-	waves.endFill();
+		waves.bezierCurveTo(points[i].x, points[i].y + startPoint, 
+							c, d,
+							e, f);
+	};
+
+	waves.lineTo(-opt.range.x, h);
+	waves.lineTo(w + opt.range.x, h);
+	// waves.endFill(); this was causing glitchiness at ends of lines i THINK 
 
 	stage.addChild(waves);
 
 };
-
 
 //
 
 
 // clear previous wave
 
+var clearStage = function(){
+	stage.clear();
+};
 var clear = function(){
 	waves.clear();
 };
@@ -141,22 +145,29 @@ var clear = function(){
 //
 
 
-
 // draw stage onto canvas
 
-
-var draw = function() {
-	requestAnimationFrame(draw);
+function draw() {
 	renderer.render(stage);
-	tick++;
-	updatePoints();
-	clear();
-	renderShapes(0, 0xFF00FF);
-	renderShapes(150, 0x9FFFFF);
-	renderShapes(300, 0xCF9FFF);
-	renderShapes(450, 0x9FFF9F);
+	requestAnimationFrame(draw);
+	renderShapes(0, 100, 10, 0xba95c6);
+	renderShapes(100, 0, 5, 0xeb3099);
+	renderShapes(220, 200, 12, 0xf45da3);
+	renderShapes(355, 0, 7, 0x5b415c);
+	renderShapes(533, 0, 1, 0xb63a9c);
+	// clearStage();
 };
 
+function move() {
+	renderer.render(stage);
+	if (tick < opt.duration) {
+		requestAnimationFrame(move);
+		updatePoints();
+		tick++;
+	};
+	clear();
+	console.log(tick);
+};
 
 var i = opt.count + 2;
 var spacing = (w + (opt.range.x * 2)) / (opt.count-1);
@@ -167,7 +178,15 @@ while(i--){
 	}));
 };
 
-draw();
+// var i = opt.count + 2;
+// var spacing = (w + (opt.range.x * 2)) / (opt.count-1);
+// while(i--){
+// 	points.push(new Point({
+// 		x: (spacing * (i - 1)) - opt.range.x,
+// 		y: h - (h * opt.level)
+// 	}));
+// };
 
 //
+
 
